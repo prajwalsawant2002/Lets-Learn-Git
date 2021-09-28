d356027c4dbc8fb7e6870639eefbbdab52bebe05
Prajwal Sawant <sawantprajwal@kccemsr.edu.in>
2021-09-28 21:36:31 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from "./Component/todoreact/Todo";
+
+const App = () => {
+    return (
+        <>
+       <Todo/>
+       </>
+    );
+};
+
+export default App
